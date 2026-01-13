9f3a8aab3506b089bf024369449d5e9d49a73204
luiscastro193 <luiscastro193@gmail.com>
2026-01-13 13:41:19 +0100

    Further optimizations

--- compile.sh
+++ compile.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-em++ beta.cpp -Ivendor -Oz -flto -fno-exceptions -fno-rtti -DNDEBUG -sENVIRONMENT=web -sEXPORT_ES6=1 -sSINGLE_FILE=1 -sSTRICT=1 -sJS_MATH=1 --closure 1 -sMINIMAL_RUNTIME=1 -sEXPORT_KEEPALIVE=1 -sINITIAL_HEAP=0 -sMALLOC=none -sEXPORTED_RUNTIME_METHODS=wasmMemory -sEXPORTED_FUNCTIONS=_set_params,_quantile,_pdfs_pointer -o beta.js
+em++ beta.cpp -Ivendor \
+	-Oz -flto -fno-exceptions -fno-rtti -DNDEBUG \
+	-sENVIRONMENT=web -sEXPORT_ES6=1 -sSINGLE_FILE=1 \
+	-sSTRICT=1 -sJS_MATH=1 -sEVAL_CTORS=2 \
+	--closure 1 -sMINIMAL_RUNTIME=1 -sEXPORT_KEEPALIVE=1 \
+	-sINITIAL_HEAP=0 -sMALLOC=none \
+	-sEXPORTED_RUNTIME_METHODS=wasmMemory -sEXPORTED_FUNCTIONS=_set_params,_quantile,_pdfs_pointer 	\
+	-o beta.js
