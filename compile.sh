5fdd3daeddda086bd826706ae847b94c918b5300
luiscastro193 <luiscastro193@gmail.com>
2026-01-23 18:25:43 +0100

    Improve SIMD

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 em++ beta.cpp -Ivendor \
-	-Oz -flto -fno-exceptions -fno-rtti -DNDEBUG -msimd128 \
+	-Oz -flto -fno-exceptions -fno-rtti -DNDEBUG -msimd128 -mavx2 \
 	-sENVIRONMENT=web -sEXPORT_ES6=1 -sSINGLE_FILE=1 --no-entry \
 	-sSTRICT=1 -sJS_MATH=1 -sEVAL_CTORS=2 \
 	--closure 1 -sMINIMAL_RUNTIME=1 -sEXPORT_KEEPALIVE=1 \
